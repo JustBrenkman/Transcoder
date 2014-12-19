5cc83cff17968ca791c32430b53eef27076be476
ben <rs068664>
2014-12-19 16:59:21 -0700

    got a basic encoding and decoding scheme going

---
+++ src/com/justb/Preferences.java
@@ -0,0 +1,52 @@
+package com.justb;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by ben on 19/12/14.
+ * <p/>
+ * JGUILibrary
+ */
+public class Preferences extends JFrame {
+    private JPanel panel;
+    private JButton okButton;
+    private JTextField textField1;
+    private JButton canelButton;
+
+    private transcoder transcoder;
+
+    public Preferences(transcoder t) {
+        super("Preferences");
+
+        this.transcoder = t;
+
+        okButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                if (textField1.getText() != null) {
+                    transcoder.changeKeyCharacter(textField1.getText().charAt(0));
+                    transcoder.updateLists();
+                    setFrameVisible(false);
+                }
+            }
+        });
+
+        canelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                setFrameVisible(false);
+            }
+        });
+
+        setContentPane(panel);
+        pack();
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void setFrameVisible(boolean b) {
+        this.setVisible(b);
+    }
+}
--- src/com/justb/transcoder.java
+++ src/com/justb/transcoder.java
@@ -3,6 +3,7 @@ package com.justb;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
 
 /**
  * Created by ben on 18/12/14.
@@ -15,6 +16,19 @@ public class transcoder extends JFrame {
     private JButton decodeButton;
     private JButton encodeButton;
     private JPanel pane;
+
+    // Menu
+    private JMenuBar menuBar;
+    private JMenu file;
+    private JMenuItem preferences;
+    private Preferences preferencesFrame;
+
+    private HashMap<Integer, Character> revisedAlphabet = new HashMap<Integer, Character>();
+    private HashMap<Character, Integer> revisedAlphabet1 = new HashMap<Character, Integer>();
+    private HashMap<Integer, Character> immutableList = new HashMap<Integer, Character>();
+    private HashMap<Character, Integer> immutableList1 = new HashMap<Character, Integer>();
+
+    private char keyCharacter = 'p';
 
 
     public transcoder() {
@@ -45,18 +59,157 @@ public class transcoder extends JFrame {
             e.printStackTrace();
         }
 
+        createMenu();
+
         setContentPane(pane);
         pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         setVisible(true);
+
+        setUpLists(immutableList, revisedAlphabet, keyCharacter);
+
+        preferencesFrame = new Preferences(this);
     }
 
     public void encode(JTextPane encode, JTextPane to) {
-        to.setText(encode.getText());
+        StringBuilder stringBuilder = new StringBuilder(encode.getText());
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < stringBuilder.length(); i++) {
+            if (stringBuilder.charAt(i) == ' ') {
+                result.append(' ');
+            } else {
+                result.append(immutableList.get(revisedAlphabet1.get(stringBuilder.charAt(i))));
+            }
+        }
+
+        to.setText(result.toString());
     }
 
     public void decode(JTextPane decode, JTextPane to) {
-        to.setText(decode.getText());
+        StringBuilder stringBuilder = new StringBuilder(decode.getText());
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < stringBuilder.length(); i++) {
+            if (stringBuilder.charAt(i) == ' ') {
+                result.append(' ');
+            } else {
+                result.append(revisedAlphabet.get(immutableList1.get(stringBuilder.charAt(i))));
+            }
+        }
+
+        to.setText(result.toString());
+    }
+
+    public void setUpLists(HashMap<Integer, Character> immutableAlphabet, HashMap<Integer, Character> revisedList, Character character) {
+        immutableAlphabet.put(0, 'a');
+        immutableAlphabet.put(1, 'b');
+        immutableAlphabet.put(2, 'c');
+        immutableAlphabet.put(3, 'd');
+        immutableAlphabet.put(4, 'e');
+        immutableAlphabet.put(5, 'f');
+        immutableAlphabet.put(6, 'g');
+        immutableAlphabet.put(7, 'h');
+        immutableAlphabet.put(8, 'i');
+        immutableAlphabet.put(9, 'j');
+        immutableAlphabet.put(10, 'k');
+        immutableAlphabet.put(11, 'l');
+        immutableAlphabet.put(12, 'm');
+        immutableAlphabet.put(13, 'n');
+        immutableAlphabet.put(14, 'o');
+        immutableAlphabet.put(15, 'p');
+        immutableAlphabet.put(16, 'q');
+        immutableAlphabet.put(17, 'r');
+        immutableAlphabet.put(18, 's');
+        immutableAlphabet.put(19, 't');
+        immutableAlphabet.put(20, 'u');
+        immutableAlphabet.put(21, 'v');
+        immutableAlphabet.put(22, 'w');
+        immutableAlphabet.put(23, 'x');
+        immutableAlphabet.put(24, 'y');
+        immutableAlphabet.put(25, 'z');
+
+        immutableList1.put('a', 0);
+        immutableList1.put('b', 1);
+        immutableList1.put('c', 2);
+        immutableList1.put('d', 3);
+        immutableList1.put('e', 4);
+        immutableList1.put('f', 5);
+        immutableList1.put('g', 6);
+        immutableList1.put('h', 7);
+        immutableList1.put('i', 8);
+        immutableList1.put('j', 9);
+        immutableList1.put('k', 10);
+        immutableList1.put('l', 11);
+        immutableList1.put('m', 12);
+        immutableList1.put('n', 13);
+        immutableList1.put('o', 14);
+        immutableList1.put('p', 15);
+        immutableList1.put('q', 16);
+        immutableList1.put('r', 17);
+        immutableList1.put('s', 18);
+        immutableList1.put('t', 19);
+        immutableList1.put('u', 20);
+        immutableList1.put('v', 21);
+        immutableList1.put('w', 22);
+        immutableList1.put('x', 23);
+        immutableList1.put('y', 24);
+        immutableList1.put('z', 25);
+
+        for (int i = immutableList1.get(character); i < 26; i++) {
+            revisedList.put(i, immutableAlphabet.get(i - immutableList1.get(character)));
+            revisedAlphabet1.put(revisedList.get(i), i);
+        }
+
+        for (int i = 0; i < immutableList1.get(character); i++) {
+            revisedList.put(i, immutableAlphabet.get((i + (26 - immutableList1.get(character)))));
+            revisedAlphabet1.put(revisedList.get(i), i);
+        }
+
+        // Print out results
+
+//        for (int i = 0; i < immutableAlphabet.size(); i++) {
+//            System.out.println(immutableAlphabet.get(i));
+//        }
+//
+//        System.out.println();
+//
+//        System.out.println(immutableList1.get(character));
+//
+//        for (int i = 0; i < 26; i++) {
+//            System.out.println(i + ": " + revisedList.get(i));
+//        }
+//
+//        for (int i = 0; i < 26; i++) {
+//            System.out.println(immutableAlphabet.get(i) + ": " + revisedAlphabet1.get(immutableAlphabet.get(i)));
+//        }
+    }
+
+    private void createMenu() {
+        menuBar = new JMenuBar();
+        file = new JMenu("File");
+        preferences = new JMenuItem("Preferences");
+
+        preferences.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                preferencesFrame.setFrameVisible(true);
+                System.out.println("Opening Preferences");
+            }
+        });
+
+        menuBar.add(file);
+        file.add(preferences);
+
+        this.setJMenuBar(menuBar);
+    }
+
+    public void changeKeyCharacter(char c) {
+        keyCharacter = c;
+    }
+
+    public void updateLists() {
+        setUpLists(immutableList, revisedAlphabet, keyCharacter);
     }
 }
